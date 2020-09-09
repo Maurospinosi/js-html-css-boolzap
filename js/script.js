eb4b708e2ddf162561a0ee6e855b1b989dc0813c
Mauro <mauro.spinosi94@gmail.com>
2020-09-09 18:54:27 +0200

    Seconda parte js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
   $(".send-message").click(
     function () {
       sendMessage();
+      setTimeout(timerFunction, 1000);
     }
   );
   $("#input-message").keyup(
     function (event) {
       if(event.which == 13){
         sendMessage();
+        setTimeout(timerFunction, 1000);
       }
     }
   );
@@ -17,20 +19,41 @@ $(document).ready(function(){
 
 
 function sendMessage() {
-  var inputText = $("#input-message").val();
-   if(inputText != ""){
-     var templatemessage = $(".template .message-row").clone();
-     var date = new Date();
-     var hours = date.getHours()
-     var minutes = date.getMinutes()
-     var time = hours + " " + minutes;
-
-     templatemessage.find(".message-text").text(inputText);
-     templatemessage.find(".message-ora").text(time);
-     templatemessage.addClass("sent");
-
-     $(".main-right").append(templatemessage);
-     $("#input-message").val("");
-
+ var inputText = $("#input-message").val();
+  if(inputText != ""){
+    var templatemessage = $(".template .message-row").clone();
+    var date = new Date();
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    if(minutes<10){
+     var time = hours + " : " + "0 " + minutes;
+    } else{
+     var time = hours + " : " + minutes;
     }
+
+
+    templatemessage.find(".message-text").text(inputText);
+    templatemessage.find(".message-ora").text(time);
+    templatemessage.addClass("send");
+
+    $(".main-right").append(templatemessage);
+    $("#input-message").val("");
+  }
+}
+
+function timerFunction (){
+  var templatemessage = $(".template .message").clone();
+  var date = new Date();
+  var hours = date.getHours()
+  var minutes = date.getMinutes()
+  if(minutes<10){
+   var time = hours + " : " + "0 " + minutes;
+  } else{
+   var time = hours + " : " + minutes;
+  }
+
+  templatemessage.find(".message-text").text("Ok");
+  templatemessage.find(".message-ora").text(time);
+
+  $(".main-right").append(templatemessage);
 }
