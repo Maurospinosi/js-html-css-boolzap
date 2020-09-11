0672384f53eebd9c4aaa92445b8f40b2ff0fa690
Mauro <mauro.spinosi94@gmail.com>
2020-09-11 16:30:42 +0200

    Seconda parte js

--- js/script.js
+++ js/script.js
@@ -3,20 +3,34 @@ $(document).ready(function(){
   $(".send-message").click(
     function () {
       sendMessage();
-      setTimeout(timerFunction, 1000);
     }
   );
   $("#input-message").keyup(
     function (event) {
       if(event.which == 13){
         sendMessage();
-        setTimeout(timerFunction, 1000);
       }
+    }
+  );
+  $("#search-name").keyup(
+    function () {
+
+      var searchInput = $(this).val();
+      var contactName = $(".access-name .contact-nome");
+
+      contactName.each(function(){
+        var names = $(this).text();
+        names = names.toLowerCase();
+        if (names.includes(searchInput) == true){
+          $(this).parents(".access-name").show();
+        } else {
+          $(this).parents(".access-name").hide();
+        }
+      });
     }
   );
 
 });
-
 
 function sendMessage() {
  var inputText = $("#input-message").val();
@@ -26,11 +40,10 @@ function sendMessage() {
     var hours = date.getHours()
     var minutes = date.getMinutes()
     if(minutes<10){
-     var time = hours + " : " + "0 " + minutes;
+     var time = hours + " : " + "0" + minutes;
     } else{
      var time = hours + " : " + minutes;
     }
-
 
     templatemessage.find(".message-text").text(inputText);
     templatemessage.find(".message-ora").text(time);
@@ -38,6 +51,7 @@ function sendMessage() {
 
     $(".main-right").append(templatemessage);
     $("#input-message").val("");
+    setTimeout(timerFunction, 1000);
   }
 }
 
@@ -47,13 +61,14 @@ function timerFunction (){
   var hours = date.getHours()
   var minutes = date.getMinutes()
   if(minutes<10){
-   var time = hours + " : " + "0 " + minutes;
+   var time = hours + " : " + "0" + minutes;
   } else{
    var time = hours + " : " + minutes;
   }
 
   templatemessage.find(".message-text").text("Ok");
   templatemessage.find(".message-ora").text(time);
+
 
   $(".main-right").append(templatemessage);
 }
